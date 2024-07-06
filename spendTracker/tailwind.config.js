11b46e0751769372e997a44cdac0918dbf619f41
ripa01 <ripabiswas2739@gmail.com>
2024-07-07 03:30:49 +0600

    setup flowbite

--- spendTracker/tailwind.config.js
+++ spendTracker/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html'
+    './templates/**/*.html',
+    './node_modules/flowbite/**/*.js'
 ],
 
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
