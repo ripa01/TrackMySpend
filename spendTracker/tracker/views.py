1493fb4115c0bbe910676e52262b8ba3448349e3
ripa01 <ripabiswas2739@gmail.com>
2024-07-09 04:08:06 +0600

    preview 3 items in recent items

--- spendTracker/tracker/views.py
+++ spendTracker/tracker/views.py
@@ -22,7 +22,7 @@ class ExpenseCreateView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['recent_expenses'] = Tracker.objects.order_by('-date')[:5]
+        context['recent_expenses'] = Tracker.objects.order_by('-date')[:3]
         return context
     
 
