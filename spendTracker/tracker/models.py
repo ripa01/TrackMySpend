e2a7d56c43c3f38a88575b720b0dbaa25c444024
ripa01 <ripabiswas2739@gmail.com>
2024-07-08 14:05:15 +0600

    Create Tracker models

--- spendTracker/tracker/models.py
+++ spendTracker/tracker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+class Tracker(models.Model):
+    category = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    payment_method = models.CharField(max_length=50, choices=[
+        ('Cash', 'Cash'), 
+        ('Credit Card', 'Credit Card'), 
+        ('Debit Card', 'Debit Card'), 
+        ('Online Payment', 'Online Payment')
+    ])
+
+    def __str__(self):
+        return self.category
+
+    
+
