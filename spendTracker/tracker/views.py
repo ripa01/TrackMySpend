f704b70969193176caae914e5e0bdfc139917758
ripa01 <ripabiswas2739@gmail.com>
2024-07-08 19:43:56 +0600

    display recently added expenses

--- spendTracker/tracker/views.py
+++ spendTracker/tracker/views.py
@@ -15,11 +15,22 @@ class ExpenseCreateView(CreateView):
     template_name = 'form.html'
     success_url = reverse_lazy('home') 
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_expenses'] = Tracker.objects.order_by('-date')[:5]
+        return context
+
+
 class ExpenseUpdateView(UpdateView):
     model = Tracker
     fields = ['category', 'amount', 'title', 'date', 'payment_method']
     template_name = 'form.html'
     success_url = reverse_lazy('home')  
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_expenses'] = Tracker.objects.order_by('-date')[:5]
+        return context
 
 class ExpenseDeleteView(DeleteView):
     model = Tracker
